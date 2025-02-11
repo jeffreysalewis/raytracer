06edbf9483514057b14973d5b9d8aa8bc7ef455c
jeffreysalewis <jeffreysalewis@gmail.com>
2025-02-11 13:07:32 -0700

    changed the ppm file to take image array

--- raytracer.cpp
+++ raytracer.cpp
@@ -5,13 +5,23 @@
 using namespace std;
 
 int main() {
+    const int width = 32;
+    const int height = 32;
+    int img[width][height][3];
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 32; j++) {
+            img[i][j][0] = 128;
+            img[i][j][1] = 128;
+            img[i][j][2] = 255;
+        }
+    }
     ofstream Render("render.ppm");
     Render << "P3\n";
-    Render << "32 32\n";
+    Render << width << " " << height << "\n";
     Render << "255\n";
-    for(int i =0; i<32; i++) {
-        for(int j=0; j<32; j++) {
-            Render << "255 0 255  ";
+    for (int i = 0; i < 32; i++) {
+        for (int j = 0; j < 32; j++) {
+            Render << img[i][j][0] << " " << img[i][j][1] << " " << img[i][j][2] << "  ";
         }
         Render << "\n";
     }
