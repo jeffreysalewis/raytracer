081905015e3e427ce0926ed28456a6e3da7d2c8b
jeffreysalewis <jeffreysalewis@gmail.com>
2025-02-11 12:26:46 -0700

    added header file and now have ppm file written out

--- raytracer.cpp
+++ raytracer.cpp
@@ -1,6 +1,21 @@
+﻿#include "raytracer.h"
+
 #include <iostream>
+#include <fstream>
 using namespace std;
 
 int main() {
+    ofstream Render("render.ppm");
+    Render << "P3\n";
+    Render << "32 32\n";
+    Render << "255\n";
+    for(int i =0; i<32; i++) {
+        for(int j=0; j<32; j++) {
+            Render << "255 0 255  ";
+        }
+        Render << "\n";
+    }
+    Render.close();
     cout << "\nthis is a ray tracer!\n" << endl;
+    return 0;
 }
